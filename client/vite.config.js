b5b0a6265c144b33df39b11feba7ba5830089081
Brian.Soto <briansoto.bs23@gmail.com>
2025-02-04 21:28:20 -0500

    fix render

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
     proxy: {
       '/graphql': {
         target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
-      }
+      },
     }
   }
 })
